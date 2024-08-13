1d5265b01dd85939d50a78a399d458cb0d6cb43a
velascor <velascor@Ubuntu.myguest.virtualbox.org>
2024-08-12 21:30:31 -0700

    fixed aesdchar package

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -1,19 +1,19 @@
 
 ##############################################################
 #
-# LDD
+# AESDCHAR
 #
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-LDD_VERSION = 7957746f09e578d7781f5106b89c46beb06d1049
+AESDCHAR_VERSION = 7957746f09e578d7781f5106b89c46beb06d1049
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LDD_SITE = git@github.com:cu-ecen-aeld/assignment-3-velascorboulder.git
-LDD_SITE_METHOD = git
+AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-velascorboulder.git
+AESDCHAR_SITE_METHOD = git
 
-LDD_MODULE_SUBDIRS = aesd-char-driver
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
 #misc_modules scull
 
 $(eval $(kernel-module))
