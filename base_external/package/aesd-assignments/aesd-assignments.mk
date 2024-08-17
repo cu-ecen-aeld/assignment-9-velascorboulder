51ca53fd64ba7b168479382ae75f93ef795a064e
velascor <velascor@Ubuntu.myguest.virtualbox.org>
2024-08-16 21:07:32 -0700

    adding modified make files

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 7957746f09e578d7781f5106b89c46beb06d1049
+AESD_ASSIGNMENTS_VERSION = 1367fa9eb560e1ffc5dca3596e6ded18b9f35f31
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
